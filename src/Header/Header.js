0866c3e64a8a238c3ecf4e6ccf751031760920ba
mathew-at-guelph <mmozaf01@uoguelph.ca>
2023-06-23 09:55:55 -0400

    Connected Help With This Page button with the 7 pages and rearranged start schema page

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,10 +3,14 @@ import { Typography, Stack, Tooltip, Button, Box } from "@mui/material";
 import { CustomPalette } from "../constants/customPalette";
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 import logo from '../assets/agri-logo.png';
+import { useNavigate } from "react-router-dom";
 
 export default function Header({ currentPage }) {
+  const navigate = useNavigate();
   const [header, setHeader] = useState(currentPage);
   const [toolTipText, setToolTipText] = useState("");
+  const [helpLink, setHelpLink] = useState("");
+
   const [headerLink] = useState(
     "https://agrifooddatacanada.ca/semantic-engine/"
   );
@@ -16,39 +20,46 @@ export default function Header({ currentPage }) {
       case "Start":
         setHeader("Start Creating an OCA Schema");
         setToolTipText("");
+        setHelpLink('/start_schema_help');
         break;
       case "Metadata":
         setHeader("Schema Metadata");
         setToolTipText(
           "This page is where you can write the metadata describing the schema you are writing. Metadata helps people find, understand, and use your schema. If you name and describe your schema descriptions in general terms, it will be easier for you and others to reuse the schema for different datasets. This will help with ensuring your data is more interoperable."
         );
+        setHelpLink('/schema_metadata_help');
         break;
       case "Details":
         setHeader("Attribute Details");
         setToolTipText(
           "Each column of your dataset is an attribute in your schema. Here you can add more details about each attribute to help people understand and use your dataset."
         );
+        setHelpLink('/attribute_details_help');
         break;
       case "Codes":
         setHeader("Add Entry Codes");
         setToolTipText(
           "Entry codes are options you want available to users as a list of choices for a specific attribute. For example, to limit gender entry to one of three choices you can use entry codes M, F, and X. Then for the English entries you can enter Male, Female and Other. If you have another language such as French, you could use Masculin, Féminin, and Autre as labels for the entry code."
         );
+        setHelpLink('/add_entry_codes_help');
         break;
       case "LanguageDetails":
         setHeader("Language Dependent Attribute Details");
         setToolTipText(
           "You can add details in each language to help users of your schema. By having languages separate from the underlying structure it means you can share your schema in multiple languages."
         );
+        setHelpLink('/language_attribute_help');
         break;
       case "View":
         setHeader("View Schema");
         setToolTipText(
           "Before finishing your schema you can preview the final contents on this page."
         );
+        setHelpLink('/view_schema_help');
         break;
       default:
         setHeader("Start Schema");
+        setHelpLink('/start_schema_help');
     }
   }, [currentPage]);
 
@@ -103,7 +114,7 @@ export default function Header({ currentPage }) {
         <Button
           color="button"
           variant="contained"
-          href={headerLink}
+
           target="_blank"
           sx={{
             m: 2,
@@ -111,6 +122,7 @@ export default function Header({ currentPage }) {
             p: 1,
             width: "15rem",
           }}
+          onClick={() => navigate(helpLink)}
         >
           Help with this page
         </Button>
