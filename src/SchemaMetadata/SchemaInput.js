7e0b4c188d91cf2f5bb0c1efeec10ec5fda67df9
mathew-at-guelph <mmozaf01@uoguelph.ca>
2023-06-27 10:22:09 -0400

    Fixed layout for UoG Schema Metadata

--- src/SchemaMetadata/SchemaInput.js
+++ src/SchemaMetadata/SchemaInput.js
@@ -69,6 +69,9 @@ export default function SchemaInput({
           height: "10.5rem",
         }}
       >
+        {language === 'English' ? <Classification /> : (
+          <Box sx={{ marginBottom: '1rem', height: '5rem' }} />
+        )}
         <Box
           sx={{
             display: "flex",
@@ -144,7 +147,6 @@ export default function SchemaInput({
             </Button>
           )}
         </Box>
-        {language === 'English' && <Classification />}
       </Box>
 
       <Box sx={{ display: "flex", flexDirection: "column" }}>
