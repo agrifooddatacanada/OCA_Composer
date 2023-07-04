4da554c9b9c433d85dffc17516d0d07e2a28eade
mathew-at-guelph <136002781+mathew-at-guelph@users.noreply.github.com>
2023-07-04 10:44:58 -0400

    Incorporate User Testing Feedback
    * Help page pop up in a new window
    
    * Placed Add Language button to the top right
    
    * Changed method of opening window from help button

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,13 +3,11 @@ import { Typography, Stack, Tooltip, Button, Box } from "@mui/material";
 import { CustomPalette } from "../constants/customPalette";
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 import logo from '../assets/agri-logo.png';
-import { useNavigate } from "react-router-dom";
 
 export default function Header({ currentPage }) {
-  const navigate = useNavigate();
   const [header, setHeader] = useState(currentPage);
   const [toolTipText, setToolTipText] = useState("");
-  const [helpLink, setHelpLink] = useState("");
+  const [helpLink, setHelpLink] = useState("start_schema_help");
 
   //Sets headers and tooltip Text based on current page
   useEffect(() => {
@@ -119,12 +117,12 @@ export default function Header({ currentPage }) {
             p: 1,
             width: "15rem",
           }}
-          onClick={() => navigate(helpLink)}
+          onClick={() => window.open(`${helpLink}`, '_blank', 'rel=noopener noreferrer')}
         >
           Help with this page
         </Button>
         <Box sx={{ color: CustomPalette.PRIMARY }}>En</Box>
       </Stack>
-    </Stack>
+    </Stack >
   );
 }
