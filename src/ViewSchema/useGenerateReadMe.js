57110c558aa7c76a7f6adbef5e394c38117ba2e6
mathew-at-guelph <mmozaf01@uoguelph.ca>
2023-10-02 12:23:18 -0400

    Updated OCA read

--- src/ViewSchema/useGenerateReadMe.js
+++ src/ViewSchema/useGenerateReadMe.js
@@ -1,7 +1,9 @@
 
 const readmeText = `
-This is a human-readable format of an OCA schema
+BEGIN_REFERENCE_MATERIAL
+******************************************************************
 OCA_READ_ME/1.0
+This is a human-readable schema, based on the OCA schema standard.
 
 Reference for Overlays Capture Architecture (OCA): 
 https://doi.org/10.5281/zenodo.7707467
@@ -9,13 +11,16 @@ https://doi.org/10.5281/zenodo.7707467
 Reference for OCA_READ_ME/1.0:
 https://github.com/agrifooddatacanada/OCA_README
 
+A schema describes details about a dataset.
 In OCA, a schema consists of a capture_base which documents the attributes and their most basic features.
 A schema may also contain overlays which add details to the capture_base.
 For each overlay and capture_base, a hash of their original contents has been calculated and is reported here as the SAID value.
 
 This README format documents the capture_base and overlays that were associated together in a single OCA Bundle.
 OCA_MANIFEST lists all components of the OCA Bundle.
-For the OCA_BUNDLE, each section between rows of ****'s contains the details of one "layer type/version" of the OCA Bundle.\n\n`;
+For the OCA_BUNDLE, each section between rows of ****'s contains the details of one "layer type/version" of the OCA Bundle.
+******************************************************************
+END_REFERENCE_MATERIAL\n\n`;
 
 
 const useGenerateReadMe = () => {
@@ -24,7 +29,7 @@ const useGenerateReadMe = () => {
     // declare the variables
     const textFile = [];
     const variablesArray = [];
-    let Manifest = [];
+    let manifest = [];
     let Layer_name = null;
     let SAID = null;
 
@@ -37,7 +42,7 @@ const useGenerateReadMe = () => {
         const files = json.files;
         const capture_base_key_value_pair = { capture_base: Object.keys(files) };
         const files_values = [capture_base_key_value_pair, ...Object.values(files)];
-        Manifest.push(files_values);
+        manifest.push(files_values);
         hasFilesProperty = true;
       }
 
@@ -66,28 +71,39 @@ const useGenerateReadMe = () => {
         ...other_variables,
       };
       variablesArray.push(variables);
+
+      // Sort the variablesArray array such that objects with a Layer_name containing meta are first
+      variablesArray.sort((a, b) => {
+        if (a.Layer_name.includes("meta") && !b.Layer_name.includes("meta")) {
+          return -1;
+        } else if (!a.Layer_name.includes("meta") && b.Layer_name.includes("meta")) {
+          return 1;
+        } else {
+          return 0;
+        }
+      });
+
       // shift the capture base to the top of the array always
-     for (let i = 0; i < variablesArray.length; i++) {
-      if (variablesArray[i].hasOwnProperty("classification")) {
-        const classifiedVariable = variablesArray.splice(i, 1)[0];
-        variablesArray.unshift(classifiedVariable);
-        break;
+      for (let i = 0; i < variablesArray.length; i++) {
+        if (variablesArray[i].hasOwnProperty("classification")) {
+          const classifiedVariable = variablesArray.splice(i, 1)[0];
+          variablesArray.unshift(classifiedVariable);
+          break;
+        }
       }
     }
-  }
-   
 
     // turning OCA bundle into OCA readme begins here
     textFile.push(
       readmeText,
       "BEGIN_OCA_MANIFEST\n",
       "**********************************************************************\n",
-      "Bundle SAID: XXXXXXXXXX\n\n"
+      "Bundle SAID: unavailable\n\n"
     );
 
     // the OCA manifest (all the overlay hashes (SAIDs))
-    const manifest_string = JSON.stringify(Manifest, null, 0);
-    const cleaned_manifest = manifest_string.replace(/[\[\]{}]/g, '').replace(/\n/g, '').replace(/,/g, ',\n').replace(/:/g, ' SAID: ');
+    const manifest_string = JSON.stringify(manifest, null, 0);
+    const cleaned_manifest = manifest_string.replace(/[[\]{}]/g, '').replace(/\n/g, '').replace(/,/g, ',\n').replace(/:/g, ' SAID: ');
     textFile.push(
       cleaned_manifest,
       "\n",
@@ -154,18 +170,21 @@ const useGenerateReadMe = () => {
 
       // adding overaly name to the textFile 
       const text_with_schema_attributes = result.replace(/Schema attribute:/g, '\nSchema attribute: ' + variable.Layer_name);
-      const text_with_schema_layer_name = text_with_schema_attributes.replace(/Layer_name:/g, 'Layer name:');
+      let text_with_schema_layer_name = text_with_schema_attributes.replace(/Layer_name:/g, 'Layer name:');
 
 
-
+      // assembling the OCA readme and chaning the schema attribute to data type
       if (!textFile.includes(text_with_schema_layer_name)) {
-        textFile.push(
-          text_with_schema_layer_name);
+        if (text_with_schema_layer_name.includes("Schema attribute: capture_base/1.0")) {
+          text_with_schema_layer_name = text_with_schema_layer_name.replace("Schema attribute: capture_base/1.0", "Schema attribute: data type");
+        }
+        // Add the text_with_schema_layer_name to the textFile.
+        textFile.push(text_with_schema_layer_name);
         textFile.push("**********************************************************************");
       }
     });
 
-    textFile.push("\nEND_OCA_BUNDLE")
+    textFile.push("\nEND_OCA_BUNDLE");
     const text = textFile.join('');
     const textBlob = new Blob([text], { type: "text/plain" });
     const downloadUrl = URL.createObjectURL(textBlob);
