b45d5576f6074d099165a958754a4589c39ac3c2
Lacheson <lyndsy@hotmail.ca>
2023-10-25 13:42:32 -0400

    padding with the accordion item Different styling depeding on the sive of screen

--- src/Landing/AccordionList.js
+++ src/Landing/AccordionList.js
@@ -1,5 +1,6 @@
 
-import { Box, Button, Typography } from '@mui/material';
+
+import { Box, Button, Typography, useMediaQuery } from '@mui/material';
 import React, { useContext } from 'react';
 import UseASchemaAccordionItem from './UseASchemaAccordionItem';
 import UseASchemaWithDataAccordionItem from './UseASchemaWithDataAccordionItem';
@@ -53,8 +54,20 @@ const AccordionList = () => {
 
   const disableButtonCheck = rawFile.length === 0 || loading === true;
 
+  const isMobile = useMediaQuery('(max-width: 767px)'); // Adjust the screen width as needed
+
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', maxWidth: '1500px', gap: 8, marginRight: 2, marginLeft: 2, marginBottom: 10 }}>
+    <Box sx={{
+      display: 'flex',
+      justifyContent: isMobile ? 'center' : 'space-between',
+      alignItems: 'center',
+      maxWidth: '1500px',
+      gap: 8,
+      marginRight: 2,
+      marginLeft: 2,
+      marginBottom: 10,
+      paddingX: 4,
+    }}>
       <Box sx={{ maxWidth: '500px', minWidth: '300px', width: "100%" }}>
         <SchemaAccordionItem />
         <WriteASchemaAccordionItem navigateToStartPage={navigateToStartPage} />
@@ -63,31 +76,22 @@ const AccordionList = () => {
         <UseASchemaAccordionItem />
         <UseASchemaWithDataAccordionItem disableButtonCheck={disableButtonCheck} handleExport={handleExport} />
       </Box>
-      {/* Added marginTop since the Quick Link is short */}
-      <Box sx={{ backgroundColor: '#ffefea', width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', marginTop: '40px' }}>
-        <Typography sx={{ fontSize: '23px', fontWeight: '400', textAlign: 'center', width: '100%', marginTop: 2 }}>Quick Links</Typography>
-        <CustomAnchorLink text="Write a schema" overrideStyle={{ fontSize: '20px', fontWeight: '500', color: CustomPalette.PRIMARY, marginLeft: 5, marginTop: 2 }} onClick={navigateToStartPage} />
-        {/* <CustomAnchorLink text="Find a schema" overrideStyle={{ fontSize: '20px', fontWeight: '500', color: CustomPalette.PRIMARY, marginLeft: 5, marginTop: 2 }} onClick={navigateToStartPage} /> */}
-        {/* <CustomAnchorLink link="https://www.semanticengine.org/#/develop" text="Parse a schema" overrideStyle={{ fontSize: '20px', fontWeight: '500', color: CustomPalette.PRIMARY, marginLeft: 5, marginTop: 2 }} /> */}
-        <Drop
-          setFile={setRawFile}
-          setLoading={setLoading}
-          loading={loading}
-          dropDisabled={dropDisabled}
-          dropMessage={dropMessage}
-          setDropMessage={setDropMessage}
-          version={1}
-        />
-        <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-          {/* <Button
-            variant="contained"
-            color="navButton"
-            onClick={() => { }}
-            sx={{ backgroundColor: CustomPalette.PRIMARY, ":hover": { backgroundColor: CustomPalette.SECONDARY }, width: '100%', maxWidth: '300px', marginTop: '30px' }}
-            disabled={disableButtonCheck}
-          >
-            Validate Schema
-          </Button> */}
+      {!isMobile && (
+        // Display "Quick Links" and buttons beside the accordion on non-mobile screens
+        <Box sx={{ backgroundColor: '#ffefea', width: '30%', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', marginRight: '20px', paddingX: 3 }}>
+          <Typography sx={{ fontSize: '23px', fontWeight: '400', textAlign: 'center', width: '100%', marginTop: 2 }}>Quick Links</Typography>
+          <CustomAnchorLink text="Write a schema" overrideStyle={{ fontSize: '20px', fontWeight: '500', color: CustomPalette.PRIMARY, marginLeft: 5, marginTop: 2 }} onClick={navigateToStartPage} />
+          <CustomAnchorLink text="Find a schema" overrideStyle={{ fontSize: '20px', fontWeight: '500', color: CustomPalette.PRIMARY, marginLeft: 5, marginTop: 2 }} onClick={navigateToStartPage} />
+          <CustomAnchorLink link="https://www.semanticengine.org/#/develop" text="Parse a schema" overrideStyle={{ fontSize: '20px', fontWeight: '500', color: CustomPalette.PRIMARY, marginLeft: 5, marginTop: 2 }} />
+          <Drop
+            setFile={setRawFile}
+            setLoading={setLoading}
+            loading={loading}
+            dropDisabled={dropDisabled}
+            dropMessage={dropMessage}
+            setDropMessage={setDropMessage}
+            version={1}
+          />
           <Button
             variant="contained"
             color="navButton"
@@ -106,7 +110,6 @@ const AccordionList = () => {
           >
             Edit Schema
           </Button>
-          {/* Added marginBottom */}
           <Button
             variant="contained"
             color="navButton"
@@ -116,25 +119,159 @@ const AccordionList = () => {
           >
             Generate Readme
           </Button>
-          {/* <Button
-            variant="contained"
-            color="navButton"
-            onClick={}
-            sx={{ backgroundColor: CustomPalette.PRIMARY, ":hover": { backgroundColor: CustomPalette.SECONDARY }, width: '100%', maxWidth: '300px', marginTop: '30px', marginBottom: '20px' }}
-            disabled={disableButtonCheck}
-          >
-            Generate Data Entry Excel
-          </Button> */}
         </Box>
-      </Box>
+      )}
     </Box>
   );
 };
 
 export default AccordionList;
 
-// Changed the structure to stakc the Accordion Items and the Quick Start links in a vertical layout so that it looks normal on a phone
-// All componnets are placed in a within a parent "box", all parameters are adjusted such that the spacing works
-// Removed individual box containers for each component
-// Put Accordion Items in a single box so they can be stacked
-// Quick links is in its own box 
+
+
+
+
+
+
+
+
+
+// import { Box, Button, Typography } from '@mui/material';
+// import React, { useContext } from 'react';
+// import UseASchemaAccordionItem from './UseASchemaAccordionItem';
+// import UseASchemaWithDataAccordionItem from './UseASchemaWithDataAccordionItem';
+// import SchemaAccordionItem from './SchemaAccordionItem';
+// import WriteASchemaAccordionItem from './WriteASchemaAccordionItem';
+// import FindASchemaAccordionItem from './FindASchemaAccordionItem';
+// import StoreASchemaAccordionItem from './StoreASchemaAccordionItem';
+// import CustomAnchorLink from '../components/CustomAnchorLink';
+// import { CustomPalette } from '../constants/customPalette';
+// import { useNavigate } from 'react-router-dom';
+// import Drop from '../StartSchema/Drop';
+// import useHandleAllDrop from '../StartSchema/useHandleAllDrop';
+// import useGenerateReadMe from '../ViewSchema/useGenerateReadMe';
+// import { Context } from '../App';
+// import useExportLogic from '../ViewSchema/useExportLogic';
+
+// const AccordionList = () => {
+//   const navigate = useNavigate();
+//   const { zipToReadme } = useContext(Context);
+//   const { toTextFile } = useGenerateReadMe();
+//   const {
+//     rawFile,
+//     setRawFile,
+//     setLoading,
+//     loading,
+//     dropDisabled,
+//     dropMessage,
+//     setDropMessage,
+//     setCurrentPage,
+//     setIsZip
+//   } = useHandleAllDrop();
+//   const { handleExport, resetToDefaults } = useExportLogic();
+
+//   const navigateToStartPage = () => {
+//     resetToDefaults();
+//     setCurrentPage('Start');
+//     navigate('/start');
+//   };
+
+//   const navigateToMetadataPage = () => {
+//     setIsZip(false);
+//     setCurrentPage('Metadata');
+//     navigate('/start');
+//   };
+
+//   const navigateToViewPage = () => {
+//     setIsZip(true);
+//     setCurrentPage('View');
+//     navigate('/start');
+//   };
+
+//   const disableButtonCheck = rawFile.length === 0 || loading === true;
+
+//   return (
+//     <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', maxWidth: '1500px', gap: 8, marginRight: 2, marginLeft: 2, marginBottom: 10 }}>
+//       <Box sx={{ maxWidth: '500px', minWidth: '300px', width: "100%" }}>
+//         <SchemaAccordionItem />
+//         <WriteASchemaAccordionItem navigateToStartPage={navigateToStartPage} />
+//         {/* <FindASchemaAccordionItem /> */}
+//         <StoreASchemaAccordionItem />
+//         <UseASchemaAccordionItem />
+//         <UseASchemaWithDataAccordionItem disableButtonCheck={disableButtonCheck} handleExport={handleExport} />
+//       </Box>
+//       {/* Added marginTop since the Quick Link is short */}
+//       <Box sx={{ backgroundColor: '#ffefea', width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', marginTop: '40px' }}>
+//         <Typography sx={{ fontSize: '23px', fontWeight: '400', textAlign: 'center', width: '100%', marginTop: 2 }}>Quick Links</Typography>
+//         <CustomAnchorLink text="Write a schema" overrideStyle={{ fontSize: '20px', fontWeight: '500', color: CustomPalette.PRIMARY, marginLeft: 5, marginTop: 2 }} onClick={navigateToStartPage} />
+//         {/* <CustomAnchorLink text="Find a schema" overrideStyle={{ fontSize: '20px', fontWeight: '500', color: CustomPalette.PRIMARY, marginLeft: 5, marginTop: 2 }} onClick={navigateToStartPage} /> */}
+//         {/* <CustomAnchorLink link="https://www.semanticengine.org/#/develop" text="Parse a schema" overrideStyle={{ fontSize: '20px', fontWeight: '500', color: CustomPalette.PRIMARY, marginLeft: 5, marginTop: 2 }} /> */}
+//         <Drop
+//           setFile={setRawFile}
+//           setLoading={setLoading}
+//           loading={loading}
+//           dropDisabled={dropDisabled}
+//           dropMessage={dropMessage}
+//           setDropMessage={setDropMessage}
+//           version={1}
+//         />
+//         <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+//           {/* <Button
+//             variant="contained"
+//             color="navButton"
+//             onClick={() => { }}
+//             sx={{ backgroundColor: CustomPalette.PRIMARY, ":hover": { backgroundColor: CustomPalette.SECONDARY }, width: '100%', maxWidth: '300px', marginTop: '30px' }}
+//             disabled={disableButtonCheck}
+//           >
+//             Validate Schema
+//           </Button> */}
+//           <Button
+//             variant="contained"
+//             color="navButton"
+//             onClick={navigateToViewPage}
+//             sx={{ backgroundColor: CustomPalette.PRIMARY, ":hover": { backgroundColor: CustomPalette.SECONDARY }, width: '100%', maxWidth: '300px', marginTop: '30px' }}
+//             disabled={disableButtonCheck}
+//           >
+//             View Schema
+//           </Button>
+//           <Button
+//             variant="contained"
+//             color="navButton"
+//             onClick={navigateToMetadataPage}
+//             sx={{ backgroundColor: CustomPalette.PRIMARY, ":hover": { backgroundColor: CustomPalette.SECONDARY }, width: '100%', maxWidth: '300px', marginTop: '30px' }}
+//             disabled={disableButtonCheck}
+//           >
+//             Edit Schema
+//           </Button>
+//           {/* Added marginBottom */}
+//           <Button
+//             variant="contained"
+//             color="navButton"
+//             onClick={() => toTextFile(zipToReadme)}
+//             sx={{ backgroundColor: CustomPalette.PRIMARY, ":hover": { backgroundColor: CustomPalette.SECONDARY }, width: '100%', maxWidth: '300px', marginTop: '30px', marginBottom: '20px' }}
+//             disabled={disableButtonCheck}
+//           >
+//             Generate Readme
+//           </Button>
+//           {/* <Button
+//             variant="contained"
+//             color="navButton"
+//             onClick={}
+//             sx={{ backgroundColor: CustomPalette.PRIMARY, ":hover": { backgroundColor: CustomPalette.SECONDARY }, width: '100%', maxWidth: '300px', marginTop: '30px', marginBottom: '20px' }}
+//             disabled={disableButtonCheck}
+//           >
+//             Generate Data Entry Excel
+//           </Button> */}
+//         </Box>
+//       </Box>
+//     </Box>
+//   );
+// };
+
+// export default AccordionList;
+
+// // Changed the structure to stakc the Accordion Items and the Quick Start links in a vertical layout so that it looks normal on a phone
+// // All componnets are placed in a within a parent "box", all parameters are adjusted such that the spacing works
+// // Removed individual box containers for each component
+// // Put Accordion Items in a single box so they can be stacked
+// // Quick links is in its own box 
--- src/Landing/Introduction.js
+++ src/Landing/Introduction.js
@@ -1,13 +1,57 @@
-import { Box, Link, Typography } from '@mui/material';
+import { Box, Link, Typography, useMediaQuery } from '@mui/material';
 import React from 'react';
 
-
 const Introduction = () => {
+  const isMobile = useMediaQuery('(max-width: 767px)'); // Adjust the screen width as needed
+
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', maxWidth: '100%', padding: '20px', color: 'white' }}>
-  
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: isMobile ? 'column' : 'row',
+        justifyContent: 'space-around',
+        maxWidth: '1500px',
+        paddingRight: 5,
+        paddingLeft: 5,
+        gap: 10,
+        color: 'white',
+      }}
+    >
+      <Box sx={{ width: isMobile ? '100%' : '50%' }}>
+        <Typography
+          sx={{
+            fontWeight: '500',
+            fontSize: isMobile ? '2rem' : '2.2rem',
+            textAlign: isMobile ? 'center' : 'left',
+            marginTop: isMobile ? '20px' : '35px',
+          }}
+        >
+          Schemas add value to data
+        </Typography>
+        <Typography sx={{ textAlign: isMobile ? 'center' : 'left', color: '#e6e6e6' }}>
+          Make your data more valuable with a data schema
+        </Typography>
+        <Box sx={{ textAlign: isMobile ? 'center' : 'left', marginTop: isMobile ? '20px' : '40px', fontSize: '1.1rem' }}>
+          <Typography>New to Schemas? Watch our video and then</Typography>
+          <Link
+            href="https://agrifooddatacanada.ca/semantic-engine/"
+            sx={{ fontSize: '1.1rem', fontWeight: '700', cursor: 'pointer', color: 'white', textDecoration: 'none' }}
+          >
+            Learn more-{'>'}
+          </Link>
+        </Box>
+        <Typography
+          sx={{
+            textAlign: isMobile ? 'center' : 'left',
+            marginTop: isMobile ? '20px' : '40px',
+            fontSize: '1.1rem',
+          }}
+        >
+          Follow our quick-start to begin writing your own data schemas.
+        </Typography>
+      </Box>
       <iframe
-        width="100%"
+        width="560"
         height="315"
         src="https://www.youtube.com/embed/r8VIIBWmL_k"
         title="YouTube video player"
@@ -15,23 +59,6 @@ const Introduction = () => {
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
       ></iframe>
-            
-      <Typography sx={{ fontWeight: '500', fontSize: '1.8rem', textAlign: 'center', marginTop: '20px' }}>
-        Schemas add value to data
-      </Typography>
-      <Typography sx={{ textAlign: 'center', color: '#e6e6e6' }}>
-        Make your data more valuable with a data schema
-      </Typography>
-      <Box sx={{ textAlign: 'center', marginTop: '20px', fontSize: '1rem' }}>
-        <Typography>New to Schemas? Watch our video and then</Typography>
-        <Link href="https://agrifooddatacanada.ca/semantic-engine/" sx={{ fontSize: '1rem', fontWeight: '700', cursor: 'pointer', color: 'white', textDecoration: 'none' }}>
-          Learn more →
-        </Link>
-      </Box>
-      <Typography sx={{ textAlign: 'center', marginTop: '20px', fontSize: '1rem' }}>
-        Follow our quick-start to begin writing your own data schemas.
-      </Typography>
-   
     </Box>
   );
 };
@@ -39,7 +66,56 @@ const Introduction = () => {
 export default Introduction;
 
 
-// Changed the placement of the iframe so that it went in the requested order
+
+
+
+
+
+
+
+
+// import { Box, Link, Typography } from '@mui/material';
+// import React from 'react';
+
+
+// const Introduction = () => {
+//   return (
+//     <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', maxWidth: '100%', padding: '20px', color: 'white' }}>
+  
+//       <iframe
+//         width="100%"
+//         height="315"
+//         src="https://www.youtube.com/embed/r8VIIBWmL_k"
+//         title="YouTube video player"
+//         frameBorder="0"
+//         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+//         allowFullScreen
+//       ></iframe>
+            
+//       <Typography sx={{ fontWeight: '500', fontSize: '1.8rem', textAlign: 'center', marginTop: '20px' }}>
+//         Schemas add value to data
+//       </Typography>
+//       <Typography sx={{ textAlign: 'center', color: '#e6e6e6' }}>
+//         Make your data more valuable with a data schema
+//       </Typography>
+//       <Box sx={{ textAlign: 'center', marginTop: '20px', fontSize: '1rem' }}>
+//         <Typography>New to Schemas? Watch our video and then</Typography>
+//         <Link href="https://agrifooddatacanada.ca/semantic-engine/" sx={{ fontSize: '1rem', fontWeight: '700', cursor: 'pointer', color: 'white', textDecoration: 'none' }}>
+//           Learn more →
+//         </Link>
+//       </Box>
+//       <Typography sx={{ textAlign: 'center', marginTop: '20px', fontSize: '1rem' }}>
+//         Follow our quick-start to begin writing your own data schemas.
+//       </Typography>
+   
+//     </Box>
+//   );
+// };
+
+// export default Introduction;
+
+
+// // Changed the placement of the iframe so that it went in the requested order
 
 
 
