ddb2be4c294ab02095357ce8b19aee0bb8dc6883
Lacheson <lyndsy@hotmail.ca>
2023-12-04 15:59:12 -0500

    Help pages updated

--- src/UsersHelp/Overlays_Help.js
+++ src/UsersHelp/Overlays_Help.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HelpPageContainer from '../components/HelpPageContainer';
 import HelpPageH2Title from '../components/HelpPageH2Title';
 import TypographyTag from '../components/TypographyTag';
+import HelpPageH3Title from '../components/HelpPageH3Title';
 
 const OverlaysHelp = () => {
   return (
@@ -13,7 +14,7 @@ const OverlaysHelp = () => {
         The following list includes the additional overlays that may be added manually to the schema.
         <br></br>
         <br></br>
-        <b>Overlays:</b>
+        <HelpPageH3Title text = 'Overlays:' />
         <u1>
           <li>Character Encoding</li>
           <li>Make Required Entries</li>
--- src/components/HelpPageH3Title.js
+++ src/components/HelpPageH3Title.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 const HelpPageH3Title = ({ text }) => {
   return (
-    <Typography variant="h3" component="h3"   sx={{ textAlign: 'left' , fontFamily:'roboto', fontSize: '3.25rem' , color: 'black', fontWeight: 'bold' }}>
+    <Typography variant="h3" component="h3"   sx={{ textAlign: 'left' , fontFamily:'roboto', fontSize: '2rem' , color: 'black', fontWeight: 'bold' }}>
       {text}
     </Typography>
   );
