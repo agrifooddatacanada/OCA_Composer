5fdaa895ab789496c116f3ba49d5fc19a57aaee2
mathew-at-guelph <136002781+mathew-at-guelph@users.noreply.github.com>
2024-02-13 11:22:46 -0500

    Added support for multiple drop files (#77)

--- src/StartSchema/useHandleAllDrop.js
+++ src/StartSchema/useHandleAllDrop.js
@@ -417,113 +417,14 @@ const useHandleAllDrop = () => {
       const reader = new FileReader();
 
       reader.onload = async (e) => {
-        const jsonFile = JSON.parse(e.target.result)?.['bundle'];
-        const languageList = [];
-        const informationList = [];
-        const labelList = [];
-        const metaList = [];
-        const entryList = [];
-        const allJSONFiles = [];
-        let loadRoot = undefined;
-        let entryCodeSummary = {};
-        let conformance = undefined;
-        let characterEncoding = undefined;
-        let loadUnits = undefined;
-        let formatRules = undefined;
-        let cardinalityData = undefined;
-
-        // load up metadata file in OCA bundle
-        if (jsonFile?.overlays?.meta) {
-          metaList.push(...jsonFile.overlays.meta);
-          languageList.push(...jsonFile.overlays.meta.map((meta) => meta.language.slice(0, 2)));
-
-          // ONLY for README
-          const readmeMeta = jsonFile.overlays.meta.map((meta) => {
-            return JSON.stringify(meta);
-          });
-          allJSONFiles.push(...readmeMeta);
+        const jsonFile = JSON.parse(e.target.result);
+        if (jsonFile?.['bundle']) {
+          handleBundleJSONDrop(jsonFile?.['bundle']);
+        } else if (jsonFile?.['schema']?.[0]) {
+          handleBundleJSONDrop(jsonFile?.['schema']?.[0]);
+        } else {
+          handleBundleJSONDrop(jsonFile);
         }
-
-        if (jsonFile?.overlays?.information) {
-          informationList.push(...jsonFile.overlays.information);
-
-          // ONLY for README
-          const readmeInformation = jsonFile.overlays.information.map((information) => {
-            return JSON.stringify(information);
-          });
-          allJSONFiles.push(...readmeInformation);
-        }
-
-        if (jsonFile?.overlays?.label) {
-          labelList.push(...jsonFile.overlays.label);
-
-          // ONLY for README
-          const readmeLabel = jsonFile.overlays.label.map((label) => {
-            return JSON.stringify(label);
-          });
-          allJSONFiles.push(...readmeLabel);
-        }
-
-        if (jsonFile?.['capture_base']) {
-          loadRoot = { ...jsonFile['capture_base'] };
-
-          // ONLY for README
-          allJSONFiles.push(JSON.stringify(loadRoot));
-        }
-
-        if (jsonFile?.overlays?.unit) {
-          loadUnits = { ...jsonFile.overlays.unit };
-
-          // ONLY for README
-          allJSONFiles.push(JSON.stringify(loadUnits));
-        }
-
-        if (jsonFile?.overlays?.conformance) {
-          conformance = { ...jsonFile.overlays.conformance };
-
-          // ONLY for README
-          allJSONFiles.push(JSON.stringify(conformance));
-        }
-
-        if (jsonFile?.overlays?.['character_encoding']) {
-          characterEncoding = { ...jsonFile.overlays['character_encoding'] };
-
-          // ONLY for README
-          allJSONFiles.push(JSON.stringify(characterEncoding));
-        }
-
-        if (jsonFile?.overlays?.entry_code) {
-          entryCodeSummary = { ...jsonFile.overlays.entry_code };
-
-          // ONLY for README
-          allJSONFiles.push(JSON.stringify(entryCodeSummary));
-        }
-
-        if (jsonFile?.overlays?.['format']) {
-          formatRules = { ...jsonFile.overlays['format'] };
-
-          // ONLY for README
-          allJSONFiles.push(JSON.stringify(formatRules));
-        }
-
-        if (jsonFile?.overlays?.entry) {
-          entryList.push(...jsonFile.overlays.entry);
-
-          // ONLY for README
-          const readmeEntry = jsonFile.overlays.entry.map((entry) => {
-            return JSON.stringify(entry);
-          });
-          allJSONFiles.push(...readmeEntry);
-        }
-
-        if (!languageList || languageList.length === 0) {
-          throw new Error('No language found in the JSON file');
-        }
-
-        processLanguages(languageList);
-        processMetadata(metaList);
-        processLabelsDescriptionRootUnitsEntries(labelList, informationList, loadRoot, loadUnits, entryCodeSummary, entryList, conformance, characterEncoding, languageList, formatRules, cardinalityData);
-        setZipToReadme(allJSONFiles);
       };
 
       reader.readAsText(acceptedFiles[0]);
@@ -542,6 +443,115 @@ const useHandleAllDrop = () => {
       }, [2500]);
     }
   }, []);
+
+  const handleBundleJSONDrop = useCallback((jsonFile) => {
+    const languageList = [];
+    const informationList = [];
+    const labelList = [];
+    const metaList = [];
+    const entryList = [];
+    const allJSONFiles = [];
+    let loadRoot = undefined;
+    let entryCodeSummary = {};
+    let conformance = undefined;
+    let characterEncoding = undefined;
+    let loadUnits = undefined;
+    let formatRules = undefined;
+    let cardinalityData = undefined;
+
+    // load up metadata file in OCA bundle
+    if (jsonFile?.overlays?.meta) {
+      metaList.push(...jsonFile.overlays.meta);
+      languageList.push(...jsonFile.overlays.meta.map((meta) => meta.language.slice(0, 2)));
+
+      // ONLY for README
+      const readmeMeta = jsonFile.overlays.meta.map((meta) => {
+        return JSON.stringify(meta);
+      });
+      allJSONFiles.push(...readmeMeta);
+    }
+
+    if (jsonFile?.overlays?.information) {
+      informationList.push(...jsonFile.overlays.information);
+
+      // ONLY for README
+      const readmeInformation = jsonFile.overlays.information.map((information) => {
+        return JSON.stringify(information);
+      });
+      allJSONFiles.push(...readmeInformation);
+    }
+
+    if (jsonFile?.overlays?.label) {
+      labelList.push(...jsonFile.overlays.label);
+
+      // ONLY for README
+      const readmeLabel = jsonFile.overlays.label.map((label) => {
+        return JSON.stringify(label);
+      });
+      allJSONFiles.push(...readmeLabel);
+    }
+
+    if (jsonFile?.['capture_base']) {
+      loadRoot = { ...jsonFile['capture_base'] };
+
+      // ONLY for README
+      allJSONFiles.push(JSON.stringify(loadRoot));
+    }
+
+    if (jsonFile?.overlays?.unit) {
+      loadUnits = { ...jsonFile.overlays.unit };
+
+      // ONLY for README
+      allJSONFiles.push(JSON.stringify(loadUnits));
+    }
+
+    if (jsonFile?.overlays?.conformance) {
+      conformance = { ...jsonFile.overlays.conformance };
+
+      // ONLY for README
+      allJSONFiles.push(JSON.stringify(conformance));
+    }
+
+    if (jsonFile?.overlays?.['character_encoding']) {
+      characterEncoding = { ...jsonFile.overlays['character_encoding'] };
+
+      // ONLY for README
+      allJSONFiles.push(JSON.stringify(characterEncoding));
+    }
+
+    if (jsonFile?.overlays?.entry_code) {
+      entryCodeSummary = { ...jsonFile.overlays.entry_code };
+
+      // ONLY for README
+      allJSONFiles.push(JSON.stringify(entryCodeSummary));
+    }
+
+    if (jsonFile?.overlays?.['format']) {
+      formatRules = { ...jsonFile.overlays['format'] };
+
+      // ONLY for README
+      allJSONFiles.push(JSON.stringify(formatRules));
+    }
+
+    if (jsonFile?.overlays?.entry) {
+      entryList.push(...jsonFile.overlays.entry);
+
+      // ONLY for README
+      const readmeEntry = jsonFile.overlays.entry.map((entry) => {
+        return JSON.stringify(entry);
+      });
+      allJSONFiles.push(...readmeEntry);
+    }
+
+    if (!languageList || languageList.length === 0) {
+      throw new Error('No language found in the JSON file');
+    }
+
+    processLanguages(languageList);
+    processMetadata(metaList);
+    processLabelsDescriptionRootUnitsEntries(labelList, informationList, loadRoot, loadUnits, entryCodeSummary, entryList, conformance, characterEncoding, languageList, formatRules, cardinalityData);
+    setZipToReadme(allJSONFiles);
+  }, [processLabelsDescriptionRootUnitsEntries, processLanguages, processMetadata, setZipToReadme]);
 
   useEffect(() => {
     if (rawFile.length > 0 && rawFile[0].size > 1000000) {
