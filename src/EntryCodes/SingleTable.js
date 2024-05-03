6c95a81e95387934672c2f1899bcbc925fee6b2e
mathew-at-guelph <136002781+mathew-at-guelph@users.noreply.github.com>
2024-05-03 15:42:34 -0400

    Added Tooltip for entry codes and fix data verifier title page (#141)

--- src/EntryCodes/SingleTable.js
+++ src/EntryCodes/SingleTable.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Tooltip, Typography } from "@mui/material";
 import { CustomPalette } from "../constants/customPalette";
 import CodeGrid from "./CodeGrid";
 import UploadFileIcon from '@mui/icons-material/UploadFile';
@@ -34,20 +34,22 @@ export default function SingleTable({ attribute, index, codeRefs, chosenTable, s
         >
           {attribute.Attribute}
         </Typography>
-        <UploadFileIcon
-          sx={{
-            color: "gray",
-            margin: "1.1rem 0 1rem 1rem",
-            ":hover": {
-              color: CustomPalette.PRIMARY,
-              cursor: "pointer",
-            }
-          }}
-          onClick={() => {
-            setChosenEntryCodeIndex(index);
-            setShowCard(true);
-          }}
-        />
+        <Tooltip title="Use an existing table (.csv format) of entry codes or upload a schema bundle containing entry codes you would like to copy.">
+          <UploadFileIcon
+            sx={{
+              color: "gray",
+              margin: "1.1rem 0 1rem 1rem",
+              ":hover": {
+                color: CustomPalette.PRIMARY,
+                cursor: "pointer",
+              }
+            }}
+            onClick={() => {
+              setChosenEntryCodeIndex(index);
+              setShowCard(true);
+            }}
+          />
+        </Tooltip>
       </Box>
       <CodeGrid index={index} codeRefs={codeRefs} chosenTable={chosenTable} setChosenTable={setChosenTable} />
     </Box>
