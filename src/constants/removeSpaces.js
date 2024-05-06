ce83c9eb63a32d6dcb80ac5d75a1a288fd2820f1
mathew-at-guelph <mmozaf01@uoguelph.ca>
2024-05-06 12:06:12 -0400

    Replaced colon in Entry Codes

--- src/constants/removeSpaces.js
+++ src/constants/removeSpaces.js
@@ -5,12 +5,28 @@ export const removeSpacesFromString = (string) => {
     return string;
   }
 };
+export const replaceColonFromString = (string) => {
+  if (typeof string === "string") {
+    return string.replace(/:/g, "-");
+  } else {
+    return string;
+  }
+};
 
 export const removeSpacesFromObject = (object) => {
   const objectCopy = JSON.parse(JSON.stringify(object));
   const keys = Object.keys(object);
   keys.forEach((key) => {
     objectCopy[key] = removeSpacesFromString(objectCopy[key]);
+  });
+  return objectCopy;
+};
+
+export const replaceColonFromObject = (object) => {
+  const objectCopy = JSON.parse(JSON.stringify(object));
+  const keys = Object.keys(object);
+  keys.forEach((key) => {
+    objectCopy[key] = replaceColonFromString(objectCopy[key]);
   });
   return objectCopy;
 };
@@ -28,6 +44,14 @@ export const removeSpacesFromObjectOfObjects = (object) => {
 export const removeSpacesFromArrayOfObjects = (objectArray) => {
   const noSpacesArray = objectArray.map((object) => {
     return removeSpacesFromObject(object);
+  });
+  return noSpacesArray;
+};
+
+export const removeSpacesAndColonFromArrayOfObjects = (objectArray) => {
+  const noSpacesArray = objectArray.map((object) => {
+    const removedString = removeSpacesFromObject(object);
+    return replaceColonFromObject(removedString);
   });
   return noSpacesArray;
 };
