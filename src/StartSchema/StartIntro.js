c92126f734cbcfa4f7f73cd42da1776d7593ee8d
Steven-Mugisha <steven.mugishamizero@mail.mcgill.ca>
2024-07-03 14:52:09 -0400

    feat: adding-create-schema-video

--- src/StartSchema/StartIntro.js
+++ src/StartSchema/StartIntro.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, useMediaQuery } from "@mui/material";
 import CustomAnchorLink from "../components/CustomAnchorLink";
 import { useTranslation } from "react-i18next";
 
 export default function StartIntro() {
+  const isMobile = useMediaQuery('(max-width: 736px)');
   const { t } = useTranslation();
 
   return (
@@ -18,11 +19,14 @@ export default function StartIntro() {
           {t('Briefly, schemas describe the attributes (variable names/column headers) of...')}
         </Box>
         <Box sx={{ m: 2 }}>
-          {t("To learn more about schemas and the OCA schema specification")} <CustomAnchorLink link="https://agrifooddatacanada.ca/semantic-engine/" text={t("read our introduction on the ADC webpage")} />.
+          {t("To learn more about schemas and the OCA schema specification")} <CustomAnchorLink link="https://agrifooddatacanada.ca/semantic-engine/" text={t("read our introduction on the ADC webpage")} />
+        </Box>
+        <Box sx={{ m: 2, textAlign: 'center' }}>
+          {t("Watch our tutorial and see an example schema being created")}
         </Box>
       </Typography>
       <br />
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/r8VIIBWmL_k" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+      <iframe width={isMobile ? '100%' : '560'} height={isMobile ? '200' : '315'} src="https://www.youtube.com/embed/ekMmpx_w45M" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
     </Box>
   );
 }
