a763381de6e32ea6f2e7638d83d18ce74e71cf54
Steven-Mugisha <steven.mugishamizero@mail.mcgill.ca>
2024-07-12 09:59:58 -0400

    hotfix: Text and Numeric regex

--- tests/matchTextRules.test.js
+++ tests/matchTextRules.test.js
@@ -129,23 +129,6 @@ test('match Regex (Short text, 250 characters max)', () => {
     });
 });
 
-
-// export const descriptionToFormatCodeText = {
-//     "": "",
-//     "Entries of any length with only capital letters": "^[A-Z]*$",
-//     "Capital or lower case letters only, at least 1 character, and 50 characters max": "^[A-Za-z]{1,50}$",
-//     "Capital or lower case letters only, 50 characters max": "^[A-Za-z]{0,50}$",
-//     "Short text, 50 characters max": "^.{0,50}$",
-//     "Short text, 250 characters max": "^.{0,250}$",
-//     "long text, 800 characters max": "^.{0,800}$",
-//     "long text, 4000 characters max": "^.{0,4000}$",
-//     "Canadian postal codes (A1A 1A1)": "^[A-Z][0-9][A-Z]\\s[0-9][A-Z][0-9]$",
-//     "Zip code": "^\\d{5,6}(?:[-\\s]\\d{4})?$",
-//     "Email address": "[a-zA-Z0-9_\\.\\+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-\\.]+",
-//     "URL": "https?\\:\\/\\/[a-zA-Z0-9\\-\\.]+\\.[a-zA-Z]{2,}",
-//     "Phone number": "\\+?\\(?\\d{2,4}\\)?[\\d\\s-]{3,}"
-//   };
-
 test('match Regex (long text, 800 characters max)', () => {
     const pattern = descriptionToFormatCodeText["long text, 800 characters max"];
     const testTexts = [
@@ -232,7 +215,6 @@ test ('match Regex (Zip code)', () => {
         expect(matchDatetime(pattern, text)).toBeFalsy();
     });
 });
-
 
 test ('match Regex (Email address)', () => {
     const pattern = descriptionToFormatCodeText["Email address"];
