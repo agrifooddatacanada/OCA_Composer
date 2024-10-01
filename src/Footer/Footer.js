c7e6fe2b793af14a69613229ed6b3b5d6fa64097
ElishaM <elishamatharu@gmail.com>
2024-10-01 17:05:12 -0400

    Add footer logo components

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -5,7 +5,9 @@ import React from 'react';
 import { themes } from '../constants/themeConstants';
 import { Stack, Divider, Box, Typography } from '@mui/material';
 
-export default function Footer({ currentPage }) {
+//export default function Footer({ currentPage }) {
+export default function Footer({ theme = 'default' }) {
+  const { logos } = themes[theme];
   return (
     <>
       <Divider orientation="horizontal" flexItem />
@@ -24,13 +26,22 @@ export default function Footer({ currentPage }) {
         >
           <div>
             <Typography sx={{ textAlign: 'left' }}>Powered by</Typography>
-            <img src={logoAgri} style={{ width: '200px', cursor: "pointer" }} alt="Agri Logo" onClick={() => (window.location.href = 'https://agrifooddatacanada.ca/')} />
+            {/* <img src={logoAgri} style={{ width: '200px', cursor: "pointer" }} alt="Agri Logo" onClick={() => (window.location.href = 'https://agrifooddatacanada.ca/')} /> */}
+            <img 
+              src={logos.agriFoodCanada_logo.url} 
+              style={logos.agriFoodCanada_logo.style} 
+              alt={logos.agriFoodCanada_logo.alt} 
+              onClick={() => (window.location.href = logos.agriFoodCanada_logo.website)} 
+            />
           </div>
 
           <div>
             <Typography sx={{ textAlign: 'left' }}>Supported by</Typography>
-            <img src={ResearchExcellentFund} style={{ height: '120px' }} alt="University of Guelph Logo"
-            // onClick={() => (window.location.href = 'https://www.uoguelph.ca/')}
+            {/* <img src={ResearchExcellentFund} style={{ height: '120px' }} alt="University of Guelph Logo" onClick={() => (window.location.href = 'https://www.uoguelph.ca/')} /> */}
+            <img 
+              src={logos.canadaFirst_logo.url} 
+              style={logos.canadaFirst_logo.style} 
+              alt={logos.canadaFirst_logo.alt} 
             />
           </div>
         </Stack>
--- src/constants/themeConstants.js
+++ src/constants/themeConstants.js
@@ -11,17 +11,25 @@ export const themes = {
       agriFoodCanada_white_logo: {
         url: '../assets/agri-logo-white.png', 
         website: "https://agrifooddatacanada.ca/",
+        alt: "Agri Logo_white",
+        style: { width: '200px', cursor: "pointer" }
       },
       agriFoodCanada_logo: {
         url: '../assets/agri-logo-white.png', 
         website: "https://agrifooddatacanada.ca/",
+        alt: "Agri Logo",
+        style: { width: '200px', cursor: "pointer" }
       },
       canadaFirst_logo: {
         url: '../assets/research-excellent-fund.png', 
+        alt: "Canada First Logo",
+        style: { height: '120px' }
       },
       omafa_ontario_logo: {
         url: "/path/to/default_logo3.png",
         website: "https://www.ontario.ca/page/ministry-agriculture-food-and-agribusiness-and-ministry-rural-affairs",
+        alt: "OMAFA Logo",
+        style: { height: '120px', marginLeft: '1rem' }
       },
     },
     
