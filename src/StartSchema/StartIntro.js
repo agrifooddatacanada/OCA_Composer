b5b79b3f8ea34e0e73b12b1b5c8d3dfb31e729ff
mathew-at-guelph <mmozaf01@uoguelph.ca>
2024-11-20 13:31:26 -0500

    Fix translation for tutorial link text

--- src/StartSchema/StartIntro.js
+++ src/StartSchema/StartIntro.js
@@ -1,37 +1,44 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { Box, Typography, useMediaQuery } from "@mui/material";
 import CustomAnchorLink from "../components/CustomAnchorLink";
-import { Trans, useTranslation } from "react-i18next";
 import CustomRouterLink from "../components/CustomRouterLink";
 
 export default function StartIntro() {
-  const isMobile = useMediaQuery('(max-width: 736px)');
+  const isMobile = useMediaQuery("(max-width: 736px)");
   const { t } = useTranslation();
 
   return (
     <Box width="80%" margin="auto">
-      <Typography
-        variant="subtitle1"
-        width="67%"
-        margin="auto"
-        textAlign={"left"}
-      >
+      <Typography variant="subtitle1" width="67%" margin="auto" textAlign="left">
         <Box sx={{ m: 2 }}>
-          {t('Briefly, schemas describe the attributes (variable names/column headers) of...')}
+          {t(
+            "Briefly, schemas describe the attributes (variable names/column headers) of..."
+          )}
         </Box>
         <Box sx={{ m: 2 }}>
-          {t("To learn more about schemas and the OCA schema specification")} <CustomAnchorLink link="https://agrifooddatacanada.ca/semantic-engine/" text={t("read our introduction on the ADC webpage")} />
+          {t("To learn more about schemas and the OCA schema specification")}{" "}
+          <CustomAnchorLink
+            link="https://agrifooddatacanada.ca/semantic-engine/"
+            text={t("read our introduction on the ADC webpage")}
+          />
         </Box>
-        <Box sx={{ m: 2, textAlign: 'center' }}>
-          <Trans
-           i18nKey={'Watch our tutorial video on creating a schema. Or read the tutorial instead.'}
-           components={{CustomLink: <CustomRouterLink to='/tutorial' text='read the tutorial' />}}>
-            Watch our tutorial video on creating a schema. Or <CustomRouterLink to='/tutorial' text='read the tutorial' /> instead.
-          </Trans>
+        <Box sx={{ m: 2, textAlign: "center" }}>
+          {t("Watch our tutorial video on creating a schema. Or")}{" "}
+          <CustomRouterLink to="/tutorial" text={t("read the tutorial")} /> {t("instead")}
+          .
         </Box>
       </Typography>
       <br />
-      <iframe width={isMobile ? '100%' : '560'} height={isMobile ? '200' : '315'} src="https://www.youtube.com/embed/ekMmpx_w45M" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+      <iframe
+        width={isMobile ? "100%" : "560"}
+        height={isMobile ? "200" : "315"}
+        src="https://www.youtube.com/embed/ekMmpx_w45M"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      />
     </Box>
   );
 }
