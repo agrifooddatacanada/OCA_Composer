55de40eb1deda09fb0e84d39a3d2ad3ee0bca6b9
Ali Asjad <aliasjadwork@gmail.com>
2025-09-18 11:57:10 -0400

    Update StepperProgressIndicator.js

--- src/StepperProgressIndicator/StepperProgressIndicator.js
+++ src/StepperProgressIndicator/StepperProgressIndicator.js
@@ -1,23 +1,37 @@
+import React from "react";
 import { Box, Step, StepLabel, Stepper } from "@mui/material";
+import { useContext } from "react";
+import { Context } from "../App";
 import { CustomPalette } from "../constants/customPalette";
+import { useTranslation } from "react-i18next";
 
 export default function StepperProgressIndicator({ activeStep, steps }) {
+  const { currentTheme } = useContext(Context);
+  const { t } = useTranslation();
   return (
     <Box sx={{ px: 10, py: 4 }}>
       <Stepper activeStep={activeStep} alternativeLabel>
-        {steps.map((step, i) => (
+        {steps.map((step) => (
           <Step
             key={step.label}
             sx={{
               "& .MuiSvgIcon-root.Mui-active": {
-                color: CustomPalette.PRIMARY
+                color: currentTheme?.primaryColor || CustomPalette.PRIMARY,
               },
               "& .MuiSvgIcon-root.Mui-completed": {
-                color: CustomPalette.PRIMARY
-              }
+                color: currentTheme?.primaryColor || CustomPalette.PRIMARY,
+              },
             }}
           >
-            <StepLabel>{step.label}</StepLabel>
+            <StepLabel
+              sx={{
+                "& .MuiStepLabel-label": {
+                  fontFamily: currentTheme?.typography?.fontFamily ?? "Roboto, sans-serif"
+                }
+              }}
+            >
+              {t(step.label)}
+            </StepLabel>
           </Step>
         ))}
       </Stepper>
