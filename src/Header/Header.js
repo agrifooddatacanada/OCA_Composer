0a2ed8ade972a9933e75e532a2167728412c790b
Setayesh Sanavi <ssanavi@uoguelph.ca>
2025-10-03 15:21:48 -0400

    Add Form Information help page

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -121,6 +121,13 @@ export default function Header({ currentPage }) {
           `https://agrifooddatacanada.github.io/OCA_Composer_help_pages/${selectedLanguage === "en-US" || selectedLanguage === "en-CA" ? "en" : selectedLanguage}/FormatText/`
         );
         break;
+      case "FormInformation":
+        setHeader(t("Add Form Information"));
+        setToolTipText("");
+        setHelpLink(
+          `https://agrifooddatacanada.github.io/OCA_Composer_help_pages/${selectedLanguage === "en-US" || selectedLanguage === "en-CA" ? "en" : selectedLanguage}/FormInformation/`
+        );
+        break;        
       case "Cardinality":
         setHeader(t("Add Entry Limit Rules for Data Entry"));
         setToolTipText("");
