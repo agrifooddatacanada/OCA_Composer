57b3f84f18fbf1ecbd8873be9ffd726a0894e428
setayesh78 <sanavisetayesh@gmail.com>
2025-10-21 12:57:46 -0400

    Add range tooltip feature

--- src/Overlays/Overlays.jsx
+++ src/Overlays/Overlays.jsx
@@ -2,13 +2,14 @@ import React, { useContext, useState } from "react";
 import { useTranslation } from "react-i18next";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
-import { Box, Button, List, ListItemButton, ListItemText } from "@mui/material";
+import { Box, Button, List, ListItemButton, ListItemText, Tooltip } from "@mui/material";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 import { CustomPalette } from "../constants/customPalette";
 import { Context } from "../App";
 import getListOfSelectedOverlays from "../constants/getListOfSelectedOverlays";
 import BackNextSkeleton from "../components/BackNextSkeleton";
 import DeleteConfirmation from "./DeleteConfirmation";
-import { shouldDisableRangeOverlay } from "../constants/utils";
+import { shouldDisableRangeOverlay, getRangeOverlayDisabledReason } from "../constants/utils";
 import { FIELD_FORMAT_OVERLAY, FIELD_RANGE_OVERLAY } from "../constants/constants";
 
 const Overlays = ({ pageBack, pageForward }) => {
@@ -28,6 +29,10 @@ const Overlays = ({ pageBack, pageForward }) => {
 
   // Convert overlay into a list of features
   const { selectedFeatures, unselectedFeatures } = getListOfSelectedOverlays(overlay);
+
+  const getDisabledReason = (featureName) =>
+  getRangeOverlayDisabledReason(featureName, selectedFeatures, attributeRowData, rangeRowData) ||
+  "";
 
   const addToSelected = (item) => {
     // Range overlay can be selected only if format overlay is selected
@@ -158,21 +163,32 @@ const Overlays = ({ pageBack, pageForward }) => {
                 }
               }}
             >
-              {unselectedFeatures.map((text) => (
-                <ListItemButton
-                  key={text}
-                  onClick={() => addToSelected(text)}
-                  disabled={shouldDisableRangeOverlay(
-                    text,
-                    selectedFeatures,
-                    attributeRowData,
-                    rangeRowData
-                  )}
-                >
-                  <AddCircleIcon sx={{ color: CustomPalette.PRIMARY }} />
-                  <ListItemText primary={t(text)} sx={{ marginLeft: 2 }} />
-                </ListItemButton>
-              ))}
+              {unselectedFeatures.map((text) => {
+                const isDisabled = shouldDisableRangeOverlay(
+                  text,
+                  selectedFeatures,
+                  attributeRowData,
+                  rangeRowData
+                );
+                const disabledReason = isDisabled ? getDisabledReason(text) : "";
+                
+                return (
+                  <Tooltip key={text} title={disabledReason} placement="right" arrow>
+                    <span>
+                      <ListItemButton
+                        onClick={() => addToSelected(text)}
+                        disabled={isDisabled}
+                      >
+                        <AddCircleIcon sx={{ color: CustomPalette.PRIMARY }} />
+                        <ListItemText primary={t(text)} sx={{ marginLeft: 2 }} />
+                        {isDisabled && (
+                          <HelpOutlineIcon sx={{ fontSize: 18, marginLeft: "6px", color: "#6b7280" }} />
+                        )}
+                      </ListItemButton>
+                    </span>
+                  </Tooltip>
+                );
+              })}
             </List>
           </Box>
         </Box>
